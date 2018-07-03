3f054f089509f7c07f3fe444a796f176d4823d3e
Derek Zhang <chromestone@users.noreply.github.com>
2018-07-03 16:15:46 -0400

    Update mp_data_generator.py
    working on loading video data

--- project/mp_data_generator.py
+++ project/mp_data_generator.py
@@ -23,6 +23,47 @@ class MPDataGenerator(TensorflowDataGenerator):
     def __init__(self, re_id_module):
 
     	self.re_id_module = re_id_module
+	self.m_len = 0
+    
+    def load_video(self, video_list_filename, labeled=True, assume_match=False):
+        """Loads all the images from a particular video list into the dataset.
+        video_list_filename: path of the file containing the list of images
+        img_dir: directory of the images
+        mask_dir: directory of the mask images, if available
+        assume_match: Whether to assume all images have ground-truth masks
+        """\
+	
+#         # Get list of images for this video
+#         video_file = open(video_list_filename, 'r')
+#         image_filenames = video_file.readlines()
+#         video_file.close()
+
+#         if image_filenames is None:
+#             print('No video list found at {}.'.format(video_list_filename))
+#             return
+
+#         # Generate images and masks
+#         for img_mask_paths in image_filenames:
+#             # Set paths and img_id
+#             if labeled:
+#                 matches = re.search('^.*\\\\(.*\\.jpg).*\\\\(.*\\.png)', img_mask_paths)
+#                 img_file, mask_file = matches.group(1, 2)
+#                 img_id = img_file[:-4]
+#             else:
+#                 matches = re.search('^([0-9a-zA-z]+)', img_mask_paths)
+#                 img_id = matches.group(1)
+#                 img_file = img_id + '.jpg'
+#                 mask_file = None
+
+#             # Check if files exist
+#             if not isfile(join(self.root_dir + '_color', img_file)):
+#                 continue
+#             if not assume_match and not isfile(join(self.root_dir + '_label', mask_file)):
+#                 mask_file = None
+
+#             # Add the image to the dataset
+#             self.add_image("WAD", image_id=img_id, path=img_file, mask_path=mask_file)
+
 
     def load_mp_data(self):
 
@@ -46,7 +87,7 @@ class MPDataGenerator(TensorflowDataGenerator):
         # Returns
             The number of batches in the Sequence.
         """
-        raise NotImplementedError
+        raise self.m_len
 
     def on_epoch_end(self):
         """Method called at the end of every epoch.
