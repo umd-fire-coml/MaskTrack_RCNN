2c903864e823dfe8fb17f24932a8b8f4e87793cd
Derek Zhang <chromestone@users.noreply.github.com>
2018-07-12 13:13:15 -0400

    Update trajectory_generator.py

--- project/trajectory_generator.py
+++ project/trajectory_generator.py
@@ -14,19 +14,10 @@ class TrajectoryDataGenerator(Sequence):
         self.re_id_module = re_id_module
         self.m_len = 0
         self.image_info = []
-        # 3-tuple list containing 1st, 2nd and end indices
-        self.video_indices = []
+        # 2-tuple list containing start and end indices of video in image_infox
+#         self.video_indices = []
         self.epoch_order = None
-        
-    def add_data(self, image_id, local_id, global_id, prev_index):
-        image_info = {
-            "id": image_id,
-            "path": path,
-            "mask_path": mask_path,
-            "prev_index": prev_index
-        }
-        self.image_info.append(image_info)
-    
+
     def load_video(self, video_list_filename):
         """Loads all the images from a particular video list into the dataset.
         video_list_filename: path of the file containing the list of images
@@ -35,63 +26,16 @@ class TrajectoryDataGenerator(Sequence):
         with open(video_list_filename, 'rb') as csvfile:
 
             reader = csv.reader(csvfile, delimiter=',')
-
-            first_row = next(reader)
-
-            # nothing to do with image
-            # instance id
-            prev_ids = {}
-            start_index = 0
-            image_info_index = 0
             
-            col_iter = iter(row)
-
-                img_id = next(row_iter)
-
-            while True:
-                try:
-                    # get the next item
-                    local_id = next(col_iter)
-                except StopIteration:
-                    # if StopIteration is raised, break from loop
-                    break
-                # expect and assume
-                global_id = next(col_iter)
-
-                prev_ids[global_id] = image_info_index
-
-                # Add to the dataset
-                self.add_data(image_id=img_id, local_id, global_id, None)
-                image_info_index += 1
-            
+#             start = m_len
             for row in reader:
-                # nothing to do with image
-                # instance id
-                curr_ids = []
+                
+                self.image_info.append(row)
+                m_len += 1
+                
+#             video_indices.append((start, m_len))
 
-                col_iter = iter(row)
-
-                img_id = next(row_iter)
-
-                while True:
-                    try:
-                        # get the next item
-                        local_id = next(col_iter)
-                    except StopIteration:
-                        # if StopIteration is raised, break from loop
-                        break
-                    # expect and assume
-                    global_id = next(col_iter)
-                    curr_ids.append((local_id, global_id))
-                    
-                    # Add to the dataset
-                    self.add_data(image_id=img_id, local_id, global_id, prev_ids[global_id])
-                    
-                 if prev_ids:
-                    pass
-                 prev_ids = curr_ids
-
-    def load_mp_data(self):
+    def load_all_videos(self, directory):
 
         pass
 
