8db941d3ce95cd81ff50a8eaccf6be9b80c5861e
Timothy Lin <tmthylin@gmail.com>
2018-07-18 14:36:39 -0400

    try with no context import

--- tests/mask_propagation_script.py
+++ tests/mask_propagation_script.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__" and __package__ is None:
     __package__ = "tests"
 
-from project.mask_propagation import MaskPropagationModule
+from .project.mask_propagation import MaskPropagationModule
 import numpy as np
 
 tst = np.empty((3000, 2000), dtype=np.float32)
